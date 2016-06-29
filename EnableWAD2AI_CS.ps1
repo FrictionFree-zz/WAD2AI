a3d13bab9c72f82248a560f7a363fd8a9b1091d0
yossia <yossia@microsoft.com>
2016-06-29 20:44:01 +0300

    Tidy up comments and variables (thank you @Harel Broitman)

--- EnableWAD2AI_CS.ps1
+++ EnableWAD2AI_CS.ps1
@@ -1,63 +1,63 @@
 ﻿#
-# This script will wire your Azure Cloud Service (Web/Worker Roles) to send diagnostics data to Microsoft Application Insights. After a successful execution of this script you will
-# be able to search and analyze both Application logs as well as Windows logs through Application Insights. You can read more about it here 
-# https://azure.microsoft.com/en-us/blog/azure-diagnostics-integration-with-application-insights/
+# This script will configure your Azure Cloud Service (Web or Worker role) to send Azure Diagnostics telemetry to Visual Studio Application Insights (WAD2AI).
+# After a successful execution of this script, you will be able to search and analyze both Application logs as well as Windows logs through Application Insights.
+# You can read more about it here: https://azure.microsoft.com/en-us/blog/azure-diagnostics-integration-with-application-insights/
 #
 
 #
 # Required parameters
 #
-$Role_name = ""                         # Name of the CS Role, should be like "WebRole1"
-$Service_Name = ""                      # short name of the cloud service, should be like "harelbr-app"  
-$AzureSubscriptionName=""               # Subscription hosting the CS (co-admin permissions are needed)
+$Role_Name = ""                         # The name of the Web or Worker Role, should be like "WebRole1"
+$Service_Name = ""                      # The name of the Cloud Service, should be like "my-service"  
+$AzureSubscription_Name=""              # The Azure subscription in which the Cloud Service was created (co-admin permissions are required)
 
 #
-# Enabling Azure Diagnostics requires a storage account to where logs will be stored (regardless of Application Insights).
-# The storage account and key do not have to match the preconfigured set.
-# Storage account can be on any subscription, not neccessarily where the VM is.
+# Enabling Azure Diagnostics requires an Azure Storage Account where logs will be stored in (regardless of Application Insights).
+# As the script cannot retrieve the Storage Account details configured for the Cloud Service, you'll need to provide the name and key of a Storage Account.
+# This can be any Storage Account, not necessarily the one used by the Cloud Service.
 #
-$Diagnostics_Storage_Name = ""    # e.g "webroleforwad"
-$Diagnostics_Storage_Key =  ""    # "(truncated)+rDzuzAyYDvwDrq4Utg0hqA=="
+$Diagnostics_Storage_Name = ""    # e.g. "webrolestorage"
+$Diagnostics_Storage_Key =  ""    # e.g. "(truncated)+rDzuzAyYDvwDrq4Utg0hqA=="
 #
-# Instrumentation Key identifies the Application Insights resource to which your logs will get sent.
+# An Instrumentation Key identifies the Application Insights resource to which your Azure Diagnostics telemetry will be sent.
 # Provide an existing Instrumentation Key or create a new one with one click at https://ms.portal.azure.com/?flight=1#blade/HubsExtension/Resources/resourceType/microsoft.insights%2Fcomponents
 
-$ApplicationInsightsInstrumentationKey =""
+$ApplicationInsights_InstrumentationKey =""
 
-If (($Service_Name -eq "") -Or ($Role_name -eq "")  -Or ($AzureSubscriptionName -eq "") -Or ($Diagnostics_Storage_Name -eq "") -Or ($Diagnostics_Storage_Key -eq "") -Or ($ApplicationInsightsInstrumentationKey -eq "") )
+If (($Service_Name -eq "") -Or ($Role_Name -eq "")  -Or ($AzureSubscription_Name -eq "") -Or ($Diagnostics_Storage_Name -eq "") -Or ($Diagnostics_Storage_Key -eq "") -Or ($ApplicationInsights_InstrumentationKey -eq "") )
 {
-    "Required run-time paramter is missing."
+    "A required paramter is missing."
     return
 }
 
 # This is where we'll keep the Azure Diagnostics Public Configuration in case you ever want to edit/reuse it
-$PublicConfigPath = ".\" + $Role_name + "_DiagPublicConfig.xml"
+$PublicConfigPath = ".\" + $Role_Name + "_DiagPublicConfig.xml"
 
-# Enter credentials manually. Can be changed to authenticate with a management cert.
+# Begin running the script. You'll be prompted to insert your Azure account's credentials manually. This can be changed to authenticate with a management certificate.
 Add-AzureAccount 
-Select-AzureSubscription $AzureSubscriptionName
-$extensionContext = Get-AzureServiceDiagnosticsExtension –ServiceName $service_name -Slot ‘Production’ -Role $role_name 
+Select-AzureSubscription $AzureSubscription_Name
+$extensionContext = Get-AzureServiceDiagnosticsExtension –ServiceName $Service_Name -Slot ‘Production’ -Role $Role_Name 
 
 
 if($extensionContext.PublicConfiguration)
 {
-    # Extract the current/existing Public Configuration and save it to disk so we can patch it
+    # Extract the current/existing Public Configuration and save it to disk so we can update it
     $publicConfiguration = '<?xml version="1.0" encoding="utf-8"?>' + "`r`n"  + $extensionContext.PublicConfiguration
     $publicConfiguration | Out-File -Encoding utf8 -FilePath $PublicConfigPath
 }
 else 
 {
-    # Enable WAD2AI with default WAD settings
+    # Enable WAD2AI with default Azure Diagnostics settings
     $PublicConfigPath = ".\DiagPublicConfigTemplateCS.xml"
-    "Since Azure Diagnostics is not currently enabled on $Role_name, we'll be using a template at $PublicConfigPath"
+    "Since Azure Diagnostics is not currently enabled on $Role_Name, we'll be using a template from $PublicConfigPath"
 }
 
-# All is set, let's go ahead and update the Public Configuration to enable ingestion of WAD into Application Insights.
-# These setting would make decent defaults for sending data to AI, but you can change the verbosity levels if you like.
-# For more info about how to edit Application Insights ingestion through Azure Diagnostics please read here
+# All is set, let's go ahead and update the Public Configuration to enable ingestion of Azure Diagnostics into Application Insights.
+# These setting would make decent defaults for sending data to Application Insights, but you can change the verbosity levels if you'd like.
+# For more info about how to edit Application Insights ingestion through Azure Diagnostics, please read here:
 # https://azure.microsoft.com/en-us/documentation/articles/azure-diagnostics-configure-applicationinsights/
 [xml] $x ='<Sink name="ApplicationInsights">
-        <ApplicationInsights>' + $ApplicationInsightsInstrumentationKey + '</ApplicationInsights>
+        <ApplicationInsights>' + $ApplicationInsights_InstrumentationKey + '</ApplicationInsights>
         <Channels>
         <Channel logLevel="Error" name="MyTopDiagData"  />
         <Channel logLevel="Verbose" name="MyLogData"  />
@@ -69,7 +69,6 @@ else
 $doc.PublicConfig.WadCfg.DiagnosticMonitorConfiguration.SetAttribute(“sinks”,”ApplicationInsights.MyLogData”)
 
 # Delete all existing sinks and recreate...
-# TBD - we might be losing a non Application Inisghts sink here, but that's quite unlikely
 if ($doc.PublicConfig.WadCfg.SinksConfig)
 {
     $doc.PublicConfig.WadCfg.RemoveChild($doc.PublicConfig.WadCfg.SinksConfig)
@@ -80,8 +79,10 @@ $child = $doc.CreateElement("SinksConfig")
 $child.SetAttribute("xmlns", ”http://schemas.microsoft.com/ServiceHosting/2010/10/DiagnosticsConfiguration")
 $child.InnerXml = $x.InnerXml
 $doc.PublicConfig.WadCfg.AppendChild($child)
+
+
 $doc.Save($PublicConfigPath)
 
 
-# Finally, update the Diagnostics Extension configuration
-Set-AzureServiceDiagnosticsExtension -DiagnosticsConfigurationPath $PublicConfigPath –ServiceName $Service_name -Slot ‘Production’ -Role $Role_name -StorageAccountName $Diagnostics_Storage_Name -StorageAccountKey $diagnostics_storage_key
+# Finally, update the Diagnostics Extension's configuration
+Set-AzureServiceDiagnosticsExtension -DiagnosticsConfigurationPath $PublicConfigPath –ServiceName $Service_Name -Slot ‘Production’ -Role $Role_Name -StorageAccountName $Diagnostics_Storage_Name -StorageAccountKey $Diagnostics_Storage_Key
